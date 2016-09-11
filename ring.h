4530842593fd5c0d0774d2de1f01dcb63267bc0e
dotSlashLu <dotslash.lu@gmail.com>
2016-09-11 16:41:49 +0800

    ring: support write over 2x larger than buffer size

--- ring.h
+++ ring.h
@@ -1,6 +1,7 @@
 typedef struct {
         int size;
         void *first;
+        // current UNUSED position
         void *curr;
 } Ring;
 typedef Ring *ringp;
@@ -8,3 +9,4 @@ typedef Ring *ringp;
 ringp ring_init(int size);
 short ring_destroy(ringp r);
 void *ring_put(ringp r, const void *, int);
+void ring_clear(ringp r);

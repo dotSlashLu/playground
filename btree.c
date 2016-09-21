1a9b0983ec61f5e7f7dd589816749c793824cf4d
Zhang Qiang <dotslash.lu@gmail.com>
2016-09-22 01:39:47 +0800

    bt

--- btree.c
+++ btree.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include "btree.h"
 
-#define PBT_ORDER 3
-#define PBT_MAX_K (PBT_ORDER - 1) * 2
-#define PBT_MIN_K PBT_ORDER - 1
-
-void pbt_put_key(bt_node_p to, bt_node_p prev, bt_node_p next)
+void pbt_put_key(bt_node_p root, int val, bt_node_p prev, bt_node_p next)
 {
         // min < nkeys < max
         // simply put into the keys
-        if (to->nkeys > PBT_MIN_K && to->nkeys < PBT_MAX_K) {
-
+        if (root->nkeys > PBT_MIN_K && root->nkeys < PBT_MAX_K) {
+                while (*root->bt_node_key++ < val)
         }
+}
+
+int main(int argc, char **argv)
+{
+        return 0;
 }
--- ring.h
+++ ring.h
@@ -7,7 +7,7 @@ typedef struct {
 } Ring;
 typedef Ring *ringp;
 
-ringp ring_init(int size);
+ringp ring_init(ringp r, int size);
 short ring_destroy(ringp r);
 void *ring_put(ringp r, const void *, int);
 void ring_clear(ringp r);
